35123eb6d28587a77fb62f9129535cb4cc959487
karthik-44 <kvrk127@gmail.com>
2023-06-28 21:09:29 -0500

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from logger import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()  #sys.exc_info function returns a 3-tuple with the exception, 
