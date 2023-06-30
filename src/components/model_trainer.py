dbb52fbd294a3928cbba8245c6f5451540797242
karthik-44 <kvrk127@gmail.com>
2023-06-29 23:01:40 -0500

    Prediction pipeline and Flask app configuration

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -38,24 +38,139 @@ class ModelTrainer:
                 "K-Neighbors" : KNeighborsRegressor(),
                 "Gradient Boost": GradientBoostingRegressor(),
                 "Adaboost": AdaBoostRegressor(),
-                "Catboost": CatBoostRegressor(),
+                "Catboost": CatBoostRegressor(verbose=False),
                 "XGboost": XGBRegressor()            
             }
 
+            params = {
+                    "Random Forest": {
+                    #"n_estimators": [10, 50, 100, 200],
+                    "max_depth": [None, 5, 10, 20],
+                    "min_samples_split": [2, 5, 10],
+                    "min_samples_leaf": [1, 2, 4]                    
+                                      },
 
-            model_report:dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
-                                               models=models)
+                    "Decision Tree": {
+                        "max_depth": [None, 5, 10, 20],
+                        "min_samples_split": [2, 5, 10],
+                        "min_samples_leaf": [1, 2, 4]
+                        
+                    },
+
+                    "Linear Regression": {
+                        
+                    },
+
+                    "K-Neighbors": {
+                        "n_neighbors": [8,16,32,64,128],
+                        "weights": ['uniform', 'distance'],
+                        "algorithm": ['auto', 'ball_tree', 'kd_tree', 'brute']
+                    },
+
+                    "Gradient Boost": {
+                        "learning_rate": [0.01, 0.1, 0.5],
+                        #"n_estimators": [8,16,32,64,128],
+                        "max_depth": [3, 5, 10],
+                        "min_samples_split": [2, 5, 10]
+                    },
+
+                    "Adaboost": {
+                        "learning_rate": [0.01, 0.1, 0.5],
+                        #"n_estimators": [8,16,32,64,128],
+                        #"base_estimator": [DecisionTreeRegressor(), RandomForestRegressor()]
+                    },
+
+                    "Catboost": {
+                        "learning_rate": [0.01, 0.1, 0.5],
+                        #"n_estimators": [8,16,32,64,128],
+                        "max_depth": [3, 5, 10],
+                        "subsample": [0.8, 1.0]
+                    },
+
+                    "XGboost": {
+                        "learning_rate": [0.01, 0.1, 0.5],
+                        #"n_estimators": [8,16,32,64,128],
+                        "max_depth": [3, 5, 10],
+                        "subsample": [0.8, 1.0],
+                        "colsample_bytree": [0.8, 1.0]
+                    }
+
+                 }
+
+            # models = {
+            #     "Random Forest": RandomForestRegressor(),
+            #     "Decision Tree": DecisionTreeRegressor(),
+            #     "Gradient Boosting": GradientBoostingRegressor(),
+            #     #"Linear Regression": LinearRegression(),
+            #     "XGBRegressor": XGBRegressor(),
+            #     "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+            #     "AdaBoost Regressor": AdaBoostRegressor(),
+            # }
+
+            # params={
+            #     "Decision Tree": {
+            #         'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+            #         # 'splitter':['best','random'],
+            #         # 'max_features':['sqrt','log2'],
+            #     },
+            #     "Random Forest":{
+            #         # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+            #         # 'max_features':['sqrt','log2',None],
+            #         'n_estimators': [8,16,32,64,128,256]
+            #     },
+            #     "Gradient Boosting":{
+            #         # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+            #         'learning_rate':[.1,.01,.05,.001],
+            #         'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+            #         # 'criterion':['squared_error', 'friedman_mse'],
+            #         # 'max_features':['auto','sqrt','log2'],
+            #         'n_estimators': [8,16,32,64,128,256]
+            #     },
+            #     #"Linear Regression":{},
+            #     "XGBRegressor":{
+            #         'learning_rate':[.1,.01,.05,.001],
+            #         'n_estimators': [8,16,32,64,128,256]
+            #     },
+            #     "CatBoosting Regressor":{
+            #         'depth': [6,8,10],
+            #         'learning_rate': [0.01, 0.05, 0.1],
+            #         'iterations': [30, 50, 100]
+            #     },
+            #     "AdaBoost Regressor":{
+            #         'learning_rate':[.1,.01,0.5,.001],
+            #         # 'loss':['linear','square','exponential'],
+            #         'n_estimators': [8,16,32,64,128,256]
+            #     },
+                
+            #         "K-Neighbors": {
+            #             "n_neighbors": [8,16,32,64,128],
+            #             "weights": ['uniform', 'distance'],
+            #             "algorithm": ['auto', 'ball_tree', 'kd_tree', 'brute']
+            #         }
+            # }
+
+            model_report, best_model = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
+                                               models=models, params = params)
             
             
             ## Find the best model score and model from the dictionary
-
-            best_model_name, best_model_score = max(model_report.items(), key=lambda x:x[1])
-            best_model = models[best_model_name]
-
-            if best_model_score < 0.6:
-                raise CustomException("No best model found")
+            ## If we execute just the below code the best model may be one of the models in models dict, say XGBRegressor() without any
+            ## hyper parameter tuning and the model.pkl file will have the model which may not be a better one.
             
-            logging.info("Best model found!")
+            # ##
+            # best_model_name, best_model_score = max(model_report.items(), key=lambda x:x[1])
+            # best_model = models[best_model_name]
+            # ##
+
+            ## So, we have to obtain the best model information after hyper-parameter tuning from the evaluate_models.
+
+
+
+            # if best_model_score < 0.6:
+            #     raise CustomException("No best model found")
+            
+            # logging.info("Best model found!")  ## checks performed in utils.py
 
             save_object(filepath=self.model_trainer_config.trained_model_file_path,
                         obj=best_model
@@ -63,6 +178,12 @@ class ModelTrainer:
             
             predicted = best_model.predict(X_test)
             r_squared = r2_score(y_test, predicted)
+
+            logging.info("The best model is {} with a model score of {}. The model is saved to {}".format(
+                best_model.__class__.__name__,
+                r_squared,
+                self.model_trainer_config.trained_model_file_path
+            ))
 
             return r_squared
 
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import dill
 import pickle
 
 from sklearn.metrics import r2_score, mean_absolute_error as mae, mean_squared_error as mse
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
 from src.exception import CustomException
 from src.logger import logging
@@ -23,31 +24,86 @@ def save_object(filepath, obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     """
     Evaluates the models based on the train, test data and provides a metric report for the models
     as a dictionary.
     """
     try:
         report = {}
+        best_models = {} #- a dictionary with best performing model from each ml algorithm.
         logging.info("Model Evaluation begun")
 
         for i in range(len(list(models))):
             logging.info("Model Evaluation for model {}".format(list(models)[i]))
 
+            #get the model and params for the model
             model = list(models.values())[i]
+            
+            param = params[list(models.keys())[i]]
+
+            # gridsearch
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
+            model.set_params(**gs.best_params_)          #best hyper-parameter tuned model
+
+            # ## randomsearch
+            # rs = RandomizedSearchCV(model, param, cv=3)
+            # rs.fit(X_train, y_train)
+            # model.set_params(**rs.best_params_)            #best hyper-parameter tuned model
+
+
             model.fit(X_train, y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
-            
+
+                       
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
             
             report[list(models.keys())[i]] = test_model_score
+
+            ## save the best model from each case
+            best_models[list(models.keys())[i]] = model
+
+            
+
         
         logging.info("Model Evaluation completed")
 
-        return report
+        ## report - contains the best scores obtained from each of the different ml algorithms.
+        ## Obtain the best model name, score from report.
+        best_model_name, best_model_score = max(report.items(), key=lambda x:x[1]) #the name, score of the model which has the highest score is returned.
+
+
+        ## best_models - a dictionary with best performing model from each ml algorithm.
+        ## extract the best of best performing models and return this along with the report
+        the_best_model = best_models[best_model_name]
+
+        if best_model_score < 0.6:
+            raise CustomException("No best model found")
+        
+        
+        logging.info("The best model is {} with a score of {}".format(best_model_name, best_model_score))
+
+        # print('#'*50)
+        # print("Report", report, sep='\n')
+        # print('#'*50)
+
+        # print('#'*50)
+        # print("Best models", best_models, sep='\n')
+        # print('#'*50)
+
+        return report, the_best_model
 
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def load_object(filepath):
+    try:
+        with open(filepath, 'rb') as filehandle:
+            return pickle.load(filehandle)
+    except Exception as e:
+        raise CustomException(e, sys)
+
